74128a8b68a003d2f06e39c91550221f2c442342
Vinh Quang Tran <starcraft6723@hotmail.com>
2022-10-01 19:00:44 +0700

    Added get album and video list

--- model/artist.py
+++ model/artist.py
@@ -14,23 +14,32 @@ artist_id_pattern = re.compile('\'artist_id\': \'[0-9]+\'')
 class Artist:
     def __init__(
         self,
-        artist_id: str
+        artist_id: str = None,
+        artist_id_number: int = None
     ):
-        """Initialize Artist.
+        """Initialize Artist. Must be initialized with artist_id or artist_id_number.
 
         Args:
             artist_id (str): Artist id.
+            artist_id_number (int): Artist id number.
 
         Raises:
+            AssertionError:
             NetworkError: If site cannot be reached
             NotFoundError: If site returns 404
             Error: Unknown errors
         """
-        self.artist_id = artist_id
+        if artist_id is None and artist_id_number is None:
+            raise AssertionError('Both artist_id and artist_id_number must not be empty')
+
+        if artist_id is not None:
+            a_id = artist_id
+        elif artist_id_number is not None:
+            a_id = artist_id_number
 
         # Parse page
         try:
-            page = requests.get(f'https://chiasenhac.vn/ca-si/{self.artist_id}.html')
+            page = requests.get(f'https://chiasenhac.vn/ca-si/{a_id}.html')
         except requests.exceptions.RequestException as e:
             logging.error(e)
             raise NetworkError
@@ -42,29 +51,34 @@ class Artist:
         # Check for error
         error_code = is_error(container)
         if error_code == '404':
-            raise NotFoundError(f'Artist id {self.artist_id} not found.')
+            raise NotFoundError(f'Artist id {a_id} not found.')
         elif error_code != False:
             raise Error(f'Unknown error. Code: {error_code}')
         
         # Get info
+        self.artist_id = page.url.split('/')[-1]
+        self.artist_id = self.artist_id[self.artist_id.rfind('-') + 1:self.artist_id.rfind('.html')]
         self.artist_name = wrapper.findChild(class_='artist_name_box').text
         self.artist_id_number = int(re.findall('\\d+', artist_id_pattern.findall(page.text)[0])[0])
 
-        logging.info(f'Getting song list of artist {self.artist_name} [{self.artist_id_number}] [{self.artist_id}].')
-        number_of_pages = int(container.findChild(id='music').find('center').text.split(' ')[-1])
-        
-        self.songs = self.get_all_songs(number_of_pages)
-
-    def get_all_songs(self, number_of_pages: int) -> set:
+    def get_all_songs(self) -> set:
         """Get all songs of an artist.
-
-        Args:
-            number_of_pages (int): Number of pages (from artist page).
 
         Returns:
             set: Song IDs.
         """
         song_ids = set()
+
+        logging.info(f'Getting song list of artist {self.artist_name} [{self.artist_id_number}] [{self.artist_id}].')
+        # Get total number of music pages
+        url = f'https://chiasenhac.vn/tab_artist?artist_id={self.artist_id_number}&tab=music'
+        page = requests.get(url)
+        if 'Chưa có bài hát nào' in page.text:
+            return song_ids
+        
+        soup = BeautifulSoup(page.text, 'html.parser')
+        pagination = soup.findAll(class_='pagination')[0]
+        number_of_pages = int(pagination.findAll('li')[-1].text)
 
         for page_idx in range(number_of_pages):
             logging.info(f'Parsing page {page_idx + 1}/{number_of_pages} of music tab of artist {self.artist_name} [{self.artist_id_number}].')
@@ -79,3 +93,69 @@ class Artist:
                 song_ids.add(extract_id(href))
 
         return song_ids
+    
+    def get_all_videos(self) -> set:
+        """Get all videos of an artist.
+
+        Returns:
+            set: Video IDs.
+        """
+        videos_ids = set()
+
+        logging.info(f'Getting video list of artist {self.artist_name} [{self.artist_id_number}] [{self.artist_id}].')
+        # Get total number of video pages
+        url = f'https://chiasenhac.vn/tab_artist?artist_id={self.artist_id_number}&tab=video'
+        page = requests.get(url)
+        if 'Chưa có video nào' in page.text:
+            return videos_ids
+        
+        soup = BeautifulSoup(page.text, 'html.parser')
+        pagination = soup.findAll(class_='pagination')[0]
+        number_of_pages = int(pagination.findAll('li')[-1].text)
+
+        for page_idx in range(number_of_pages):
+            logging.info(f'Parsing page {page_idx + 1}/{number_of_pages} of video tab of artist {self.artist_name} [{self.artist_id_number}].')
+            url = f'https://chiasenhac.vn/tab_artist?page={page_idx + 1}&artist_id={self.artist_id_number}&tab=video'
+            page = requests.get(url)
+            soup = BeautifulSoup(page.text, 'html.parser')
+
+            videos = soup.findAll(class_='card-title')
+            for video in videos:
+                # Get video id
+                href = video.find('a')['href']
+                videos_ids.add(extract_id(href))
+
+        return videos_ids
+
+    def get_all_albums(self) -> set:
+        """Get all albums of an artist.
+
+        Returns:
+            set: Album IDs.
+        """
+        albums_ids = set()
+
+        logging.info(f'Getting album list of artist {self.artist_name} [{self.artist_id_number}] [{self.artist_id}].')
+        # Get total number of album pages
+        url = f'https://chiasenhac.vn/tab_artist?artist_id={self.artist_id_number}&tab=album'
+        page = requests.get(url)
+        if 'Chưa có album nào' in page.text:
+            return albums_ids
+
+        soup = BeautifulSoup(page.text, 'html.parser')
+        pagination = soup.findAll(class_='pagination')[0]
+        number_of_pages = int(pagination.findAll('li')[-1].text)
+
+        for page_idx in range(number_of_pages):
+            logging.info(f'Parsing page {page_idx + 1}/{number_of_pages} of album tab of artist {self.artist_name} [{self.artist_id_number}].')
+            url = f'https://chiasenhac.vn/tab_artist?page={page_idx + 1}&artist_id={self.artist_id_number}&tab=album'
+            page = requests.get(url)
+            soup = BeautifulSoup(page.text, 'html.parser')
+
+            albums = soup.findAll(class_='card-title')
+            for album in albums:
+                # Get album id
+                href = album.find('a')['href']
+                albums_ids.add(extract_id(href))
+
+        return albums_ids
